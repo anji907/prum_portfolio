eca5b1ba8e8eab887763d659a561b10e312bef0c
anji907 <anji.sk129@gmail.com>
2023-10-14 14:48:52 +0900

    chartにデータを渡す処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,36 @@ class UsersController < ApplicationController
 
   def profile
     @user = current_user
+
+    @backend_2months_ago = Item.where(
+                                created_at: Time.current.months_ago(2).beginning_of_month..Time.current.months_ago(2).end_of_month,
+                                category_id: 1, user_id: current_user.id).pluck(:learning_time).sum
+    @backend_last_month = Item.where(
+                                created_at: Time.current.months_ago(1).beginning_of_month..Time.current.months_ago(1).end_of_month,
+                                category_id: 1, user_id: current_user.id).pluck(:learning_time).sum
+    @backend_this_month = Item.where(
+                                created_at: Time.current.beginning_of_month..Time.current.end_of_month,
+                                category_id: 1, user_id: current_user.id).pluck(:learning_time).sum
+
+    @frontend_2months_ago = Item.where(
+                                created_at: Time.current.months_ago(2).beginning_of_month..Time.current.months_ago(2).end_of_month,
+                                category_id: 2, user_id: current_user.id).pluck(:learning_time).sum
+    @frontend_last_month = Item.where(
+                                created_at: Time.current.months_ago(1).beginning_of_month..Time.current.months_ago(1).end_of_month,
+                                category_id: 2, user_id: current_user.id).pluck(:learning_time).sum
+    @frontend_this_month = Item.where(
+                                created_at: Time.current.beginning_of_month..Time.current.end_of_month,
+                                category_id: 2, user_id: current_user.id).pluck(:learning_time).sum
+
+    @infra_2months_ago = Item.where(
+                                created_at: Time.current.months_ago(2).beginning_of_month..Time.current.months_ago(2).end_of_month,
+                                category_id: 3, user_id: current_user.id).pluck(:learning_time).sum
+    @infra_last_month = Item.where(
+                                created_at: Time.current.months_ago(1).beginning_of_month..Time.current.months_ago(1).end_of_month,
+                                category_id: 3, user_id: current_user.id).pluck(:learning_time).sum
+    @infra_this_month = Item.where(
+                                created_at: Time.current.beginning_of_month..Time.current.end_of_month,
+                                category_id: 3, user_id: current_user.id).pluck(:learning_time).sum
   end
 
   private
