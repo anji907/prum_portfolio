f570aafe695f9f1ba13959592e9b7376188f72c3
anji907 <anji.sk129@gmail.com>
2023-10-19 12:30:38 +0900

    jsファイルの追加

---
+++ app/javascript/custom/select_option.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbo:load', () => {
+  document.getElementById('custom_select').addEventListener('change', function(e) {
+    let selectedValue = this.value;
+
+    window.location.href = 'http://localhost:3000/items/?selected_value=' + selectedValue;
+  });
+
+  // 現在のURLを取得
+  const currentUrl = window.location.href;
+  const urlParams = new URLSearchParams(new URL(currentUrl).search);
+  const selectedValue = urlParams.get('selected_value');
+  const optionElement = document.querySelector(`option[value="${selectedValue}"]`);
+  optionElement.setAttribute('selected', 'selected');
+});
+
