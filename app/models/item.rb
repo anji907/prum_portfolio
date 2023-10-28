cc03fe09220f46004e82667705b7ebd3c21e09c7
anji907 <anji.sk129@gmail.com>
2023-10-28 16:57:28 +0900

    Itemにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, presence: true
+  validates :learning_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
